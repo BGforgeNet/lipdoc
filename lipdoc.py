78d99ba02affaf6c4c7eeec1b7228720bcc19a80
egornovivan <egornov.ivan@gmail.com>
2021-08-14 20:29:16 +0500

    initial commit

---
+++ lipdoc.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys, os
+import re
+
+parser = argparse.ArgumentParser(description='Doubles the sample rate of a lip file', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('ifile', help='input lip file')
+parser.add_argument('-r', '--reverse', action='store_true', help='reverse', default=False)
+args = parser.parse_args()
+
+ifile = args.ifile
+reverse = args.reverse
+if not re.search(r"\.lip$", ifile.lower()):
+  print("INVALID FILE EXTENSION\nWARNING, YOU COULD DAMAGE YOUR FILES")
+  exit(1)
+
+acm_length_off = 16
+acm_length_len = 4
+acm_offset_off = 187
+acm_offset_len = 4
+marker_len = 4
+file_len = os.stat(ifile).st_size
+
+def read_double_write(fh, foff, flen, rev):
+  fh.seek(foff)
+  fval = (int.from_bytes(fh.read(flen), byteorder='big', signed=False))
+  if rev and fval:
+    fval = int(fval/2)
+  elif not rev:
+    fval = int(fval*2)
+  fh.seek(foff)
+  fh.write((fval.to_bytes(flen, byteorder='big', signed=False)))
+
+with open(ifile, 'r+b') as lip:
+  lip.seek(acm_offset_off)
+  if (lip.read(marker_len + acm_offset_len)) == (b'\x00\x00\x00\x01\x00\x00\x00\x00'):
+    read_double_write(lip, acm_length_off, acm_length_len, reverse)
+    while(True):
+      acm_offset_off += marker_len
+      if acm_offset_off >= file_len:
+        print("EOF!")
+        break
+      read_double_write(lip, acm_offset_off, acm_offset_len, reverse)
+      acm_offset_off += acm_offset_len
+  else:
+    print("unknown marker and offset, please tell the developer")
+    exit(1)
+exit(0)
+
+
